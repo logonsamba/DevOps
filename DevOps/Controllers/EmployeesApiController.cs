7d202a8b72977673b95e817339120dcf3a52fa61
sambasivarao morla <sambasivarao.morla@aenetworks.com>
2020-02-21 14:31:41 +0530

    New Application Commit

---
+++ DevOps/Controllers/EmployeesApiController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using DevOps.Models;
+
+namespace DevOps.Controllers
+{
+    [RoutePrefix("Employee")]
+    public class EmployeesApiController : ApiController
+    {
+        private DevOpsEntities db = new DevOpsEntities();
+
+        [Route("GetEmployee")]
+        // GET: api/EmployeesApi
+        public IQueryable<Employee> GetEmployees()
+        {
+            return db.Employees;
+        }
+        [Route("GetEmployeeById")]
+        // GET: api/EmployeesApi/5
+        [ResponseType(typeof(Employee))]
+        public async Task<IHttpActionResult> GetEmployee(int id)
+        {
+            Employee employee = await db.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employee);
+        }
+        [Route("PutEmployee")]
+        // PUT: api/EmployeesApi/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutEmployee(int id, Employee employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != employee.EmpID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(employee).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+        [Route("PostEmployee")]
+        // POST: api/EmployeesApi
+        [ResponseType(typeof(Employee))]
+        public async Task<IHttpActionResult> PostEmployee(Employee employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Employees.Add(employee);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = employee.EmpID }, employee);
+        }
+
+        [Route("DeleteEmployee")]
+        // DELETE: api/EmployeesApi/5
+        [ResponseType(typeof(Employee))]
+        public async Task<IHttpActionResult> DeleteEmployee(int id)
+        {
+            Employee employee = await db.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            db.Employees.Remove(employee);
+            await db.SaveChangesAsync();
+
+            return Ok(employee);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool EmployeeExists(int id)
+        {
+            return db.Employees.Count(e => e.EmpID == id) > 0;
+        }
+    }
+}
